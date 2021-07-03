72095153a8a2b5388704c2a6ab1ccc3183e10f5c
Hrishabh8080 <hrishabh8080@gmail.com>
2021-07-03 13:15:35 +0530

    d

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require("body-parser");
 const port = 8080
-// const { data } = require('./data')
+const { data } = require('./data')
 // Parse JSON bodies (as sent by API clients)
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
